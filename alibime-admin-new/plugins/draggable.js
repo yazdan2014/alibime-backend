62658a10b4d1a014ea0d2b699cdd76bde27b01b2
Yaz <yazeinal@gmail.com>
2022-09-11 13:24:22 +0430

    sdf

--- alibime-admin-new/plugins/draggable.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-
-import VueDraggable from "vue-draggable";
-Vue.use(VueDraggable);
--- alibime-admin-new/store/users/mutations.js
+++
@@ -1,9 +0,0 @@
-export default {
-  getsAllUsers(state, users) {
-    state.state = users;
-  }
-};
-
-function orderFixes(orders, rootState) {
-  orders.forEach(element => {});
-}
